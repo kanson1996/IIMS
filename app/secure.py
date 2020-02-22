4612e3f4ce7b3f6c49a46e26112aad8254bc0592
kanson1996 <935017625@qq.com>
2020-02-22 19:12:19 +0800

    1.修复因代码整合而导致的包名错误 2.数据库连接池从pymysql切换为mysql-connector

--- app/secure.py
+++ app/secure.py
@@ -2,6 +2,6 @@
 # -*- coding: utf-8 -*-
 
 DEBUG = True # False
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:123456@127.0.0.1:3306/iims?charset=utf8'
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:123456@localhost:3306/iims?charset=utf8'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY ='Q\xaef\xabI \x14\xd4\x83\xb5zSd\xa8?\xe5{\xa9\x003\xec\x95\xd8h \xde\x83\x0f\x94\xaf8`'
